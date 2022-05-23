8211282d60a5d504194b125ca8089b546b4526f1
aiwa1986 <tatiana.belova@nmbu.no>
2022-05-23 10:11:47 +0200

    package requirements added

--- R/run_pca.R
+++ R/run_pca.R
@@ -8,7 +8,7 @@
 run_pca <- function(data) {
     data_t <- Matrix::t(data)
     # Perform scaling and centering of the data
-    res_pca <- prcomp(data_t, scale. = T, center = T)
+    res_pca <- stats::prcomp(data_t, scale. = T, center = T)
     # Extract variance explained by the first PC
     pc1 <- summary(res_pca)$importance[2, 1] * 100
     pca_result <- data.frame("pc1" = pc1, "n_edges" = nrow(data))
